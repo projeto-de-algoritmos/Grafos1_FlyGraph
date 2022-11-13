0d7c54e22931dacaa7983a781b013e8a29ede06d
Victor Buendia <victorbuendia03@gmail.com>
2022-11-13 14:03:42 -0300

    Fazendo o BFS retornar a árvore de nós em vez de apenas o nome dos nós

--- graphs.py
+++ graphs.py
@@ -1,7 +1,10 @@
 import sys
+import copy
+
 sys.path.insert(0, "models")
 sys.path.insert(0, "data")
 
+from queue import Queue
 from edge import Aresta
 from node import No
 
@@ -26,3 +29,43 @@ def imprimeGrafo(nodesList):
 		for edge in node.la:
 			edgeList = edgeList + edge.f + ' '
 		print(f"Nó({node.id}) -> LA: [ {edgeList}]")
+
+
+def bfs(nodesList, source, end):
+    visited = {}
+    parent = {}
+    nodes = {}
+    queue = Queue()
+
+    for node in nodesList:
+        visited[node.id] = False
+        parent[node.id] = None
+        nodes[node.id] = node
+
+    # print(visited)
+    # print(parent)
+    # print(nodes)
+
+    visited[source]= True
+    n = next((x for x in nodesList if x.id == source), None)
+    queue.put(n)
+
+    for node in nodesList:
+        if not visited[node.id]:           
+            
+            while not queue.empty():
+                u = queue.get()
+                for v in u.la:
+                    if not visited[v.f]:
+                        parent[v.f] = u.id
+                        visited[v.f] = True
+                        n = next((x for x in nodesList if x.id == v.f), None)
+                        queue.put(n)
+
+    path = []
+    if parent[end] != None:
+        while end is not None:
+            path.append(copy.copy(nodes[end]))
+            end = parent[end]
+        path.reverse()
+        return path
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pandas import *
 from graphs import *
-from queue import Queue
+
 
 if __name__ == "__main__":
     nodesList = []
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     criaNos(nodesList=nodesList, nodes=nodes)
     criaArestas(nodesList=nodesList, edges=edges)
-    imprimeGrafo(nodesList=nodesList)
+    # imprimeGrafo(nodesList=nodesList)
 
     # xls = ExcelFile("./data/pessoas.xlsx")
     # nodes = xls.parse(xls.sheet_names[0]).to_dict()
@@ -22,41 +22,9 @@ if __name__ == "__main__":
     # imprimeGrafo(nodesList=nodesList)
 
 
-def bfs(source, end):
-    visited = {}
-    parent = {}
-    queue = Queue()
-
-    for node in nodesList:
-        visited[node.id] = False
-        parent[node.id] = None
-
-    visited[source]= True
-    n = next((x for x in nodesList if x.id == source), None)
-    queue.put(n)
-
-    for node in nodesList:
-        if not visited[node.id]:           
-            
-            while not queue.empty():
-                u = queue.get()
-                for v in u.la:
-                    if not visited[v.f]:
-                        parent[v.f] = u.id
-                        visited[v.f] = True
-                        print(u.id)
-                        n= next((x for x in nodesList if x.id == v.f), None)
-                        queue.put(n)
-
-    path = []
-    print(parent)
-    if parent[end] != None:
-        while end is not None:
-            path.append(end)
-            end = parent[end]
-        path.reverse()
-        print(path)
-    else:
-        print("Não existe caminho")
-
-bfs("A", "C")
+fp = bfs(nodesList, "F", "G")
+names = []
+if fp is not None:
+    for person in fp:
+        names.append(person.id)
+print("Não há caminho" if fp is None else names)
