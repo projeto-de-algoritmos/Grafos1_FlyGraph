24f12ead22d7e01717f315f5b11f9d8de73c3e26
Jv Correia <correia.jv16@gmail.com>
2022-11-17 17:42:56 -0300

    Consumindo endpoint get-data

--- djangoConfig/frontend/src/App.js
+++ djangoConfig/frontend/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 function App() {
+  const [flight, setFlights] = useState([]);
+  const [totalPrice, setTotalPrice] = useState("");
+
+  const getFligths = async (ido, idd) => {
+    try {
+      const _flight = await api.get(`get-data/?ido=${ido}&idd=${idd}`);
+      setFlights(_flight.data.flights);
+      setTotalPrice(_flight.data.total_price);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  let i = 0;
+  function soma() {
+    i++;
+  }
+
+  useEffect(() => {
+    getFligths(5, 9);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Teste 123 lala
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Configurando o react
-        </a>
-      </header>
+      {flight &&
+        flight.map((data) => {
+          soma();
+          return (
+            <div>
+              <p>Vôo {i}</p>
+              Origem: {data.origin.town}
+              <br />
+              Destino: {data.destination.town}
+              <br />
+              Preço: {data.price}
+            </div>
+          );
+        })}
+      <p> Preço total: {totalPrice}</p>
     </div>
   );
 }
